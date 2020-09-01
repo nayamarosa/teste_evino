8e7134dfe74ce5f19a1ca4ecae27aeec22d6839b
NayamaRosa <falecom@nayama.com.br>
2020-09-01 08:38:40 -0300

    init home route

---
+++ src/routes/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+import './Home.scss';
+
+import { ListContext } from '../../containers/Context';
+import List from '../../containers/List';
+
+const Home = () => {
+  const list = useContext(ListContext)
+
+  return (
+    <List list={list}/>
+  )
+}
+
+export default Home;
