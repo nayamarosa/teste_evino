d53db8cb796cda209fb99ff37a3e6ce6ddd02e03
NayamaRosa <falecom@nayama.com.br>
2020-09-01 18:33:47 -0300

    [context] adjust context product detail

--- src/containers/ProductDetail/ProductDetail.jsx
+++ src/containers/ProductDetail/ProductDetail.jsx
@@ -1,54 +1,63 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { useHistory } from "react-router-dom";
+
+import Button from '../../components/Button/Button';
 
 const ProductDetail = ({productSelected}) => {
-  const [productDetail, setProductDetail] = useState({});
-
-  useEffect(() => {
-    let productDetail = productSelected[0];
-    setProductDetail(productDetail)
-  }, [productSelected])
+  const { images, name, prices, aboutThisWine, type, producer } = productSelected
 
   const formatPrice = (price) => {
     let beforeComma = price.toString().slice(0, -2);
     return `${beforeComma},${price % 100}`;
   }
 
-  console.log(productDetail)
+  let history = useHistory();
+  const handleBackBtn = (e) => {
+    e.preventDefault(); 
+    history.push('/');
+  }
   
   return (    
     <>
     {
-      productDetail !== undefined 
+      productSelected !== undefined 
       ? <section className="product">
           <div className="product__image">
             {
-              productDetail.images !== undefined
-              ? <img src={productDetail.images.large} alt={productDetail.name}/>
-              : <p>imagem não encontrada</p>
+              images !== undefined
+              ? <img src={images.large} alt={name}/>
+              : <p>Imagem não encontrada</p>
             }
           </div>
           <article className="container product__info">
-            <h2>{productDetail.name}</h2>
+            <h2>{name}</h2>
             {
-              productDetail.prices !== undefined
-              ? <p>R$ <span>{formatPrice(productDetail.prices.recommended)}</span></p>
-              : <p>preço não disponível</p>
+              prices !== undefined
+              ? <p>R$ <span>{formatPrice(prices.recommended)}</span></p>
+              : <p className="product__info-undefined">Preço não disponível</p>
             }
-            <h3>{productDetail.aboutThisWine}</h3>
+            <h3>{aboutThisWine}</h3>
             <div className="product__info-location">
-              <p>Tipo: <span>{productDetail.type}</span></p>
+              <p>Tipo: <span>{type}</span></p>
               {
-                productDetail.producer !== undefined
+                producer !== undefined
               ? <>
-                  <p>Origem: <span>{productDetail.producer.country}</span></p>
-                  {/* <img src={productDetail.countries.map(country => country.icon)} alt={productDetail.producer.country}/> */}
+                  <p>Origem: <span>{producer.country}</span></p>
                 </>
-              : <p>produtor desconhecido</p>
+              : <p className="product__info-undefined">Produtor desconhecido</p>
               }
             </div>
+          <div className="product__btn-back">
+            <Button 
+              id="back"
+              text="Voltar"
+              classNameBtn="btn__primary"
+              onClick={(e) => handleBackBtn(e)}
+            />
+          </div>
           </article>
         </section>
-      : <p>produto não encontrado</p>
+      : <p className="product__not-found">Produto não encontrado</p>
     }
     </>
   )
