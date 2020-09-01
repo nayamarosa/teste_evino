0c92c8c21625b35378f8a6398b90d2bcbd636c14
NayamaRosa <falecom@nayama.com.br>
2020-09-01 18:34:46 -0300

    [context] adjust context route

--- src/routes/Product/Product.jsx
+++ src/routes/Product/Product.jsx
@@ -3,24 +3,37 @@ import { useLocation } from "react-router-dom";
 
 import { ListContext } from '../../containers/Context';
 
+import Loading from '../../components/Loading';
 import ProductDetail from '../../containers/ProductDetail';
 
 const Product = () => {
   const list = useContext(ListContext);
-  const [productSelected, setproductSelected] = useState([]);
+  const [productSelected, setproductSelected] = useState({});
 
   let location = useLocation();
   useEffect(() => {
     let pathname = location.pathname;
-    let productUrl = pathname.split("/")[2];
-    const filter = list.filter((item) => item.url.includes(productUrl));
-    
-    setproductSelected(filter)     
-  }, [location.pathname, list])
+    let url = pathname.split("/")[2];
+    async function fetchProduct() {
+      try {
+        await fetch(` https://api.evino.com.br/catalog/v2/product/${url}`)
+        .then(response => response.json())
+        .then(data => setproductSelected(data.data))
+      } catch (e) {
+        console.error(e);
+        throw "Serviço indisponível, tente novamente mais tarde"
+      }
+    };
+    fetchProduct();
+  }, []);
 
   return (
     <main>
-      <ProductDetail productSelected={productSelected}/>
+      {
+        productSelected === {}
+        ? <Loading />
+        : <ProductDetail productSelected={productSelected}/>
+      }
     </main>
   )
 }
