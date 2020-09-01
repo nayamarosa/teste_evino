266e8520978a919cf73281a8ea4eb12ded907d3f
NayamaRosa <falecom@nayama.com.br>
2020-09-01 16:37:31 -0300

    [product][route][css] reouto to product and style product detail

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Home from './Home';
+import Product from './Product';
 
 const Routes = () => (
   <Switch>
     <Route exact path="/">
       <Home />
     </Route>
-    <Route exact path="/produto/:producturl">
-      <Home />
+    <Route path="/product/:producturl">
+      <Product />
     </Route>
   </Switch>
 );
